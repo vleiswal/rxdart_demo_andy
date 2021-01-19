ebe10e38c7f94c25595f5b6305bf31b32e4ae419
vleiswal <vleiswal@gmail.com>
2021-01-19 13:40:29 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/rxdart_andy_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rxdart_andy_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/launch.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Launch extends StatefulWidget {
+  @override
+  _LaunchState createState() => _LaunchState();
+}
+
+class _LaunchState extends State<Launch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
